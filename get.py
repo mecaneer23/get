0019de4a5e6c3ec7f85dc348eafd0be6166ead89
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-08-31 11:10:03 -0500

    feat: add cli assistant

--- get.py
+++ get.py
@@ -10,7 +10,20 @@ def parse_args() -> Namespace:
     Parse command line arguments. Use --help for help
     """
     parser = ArgumentParser()
-    parser.add_argument("url", type=str, help="Location to download from")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "url",
+        type=str,
+        nargs="?",
+        help="Location to download from",
+    )
+    group.add_argument(
+        "--assist",
+        "-a",
+        action="store_true",
+        help="Launch a CLI helper tool to autofill parts of\
+            a url for you",
+    )
     parser.add_argument(
         "path",
         type=str,
@@ -19,7 +32,6 @@ def parse_args() -> Namespace:
         help="Optional file path to write to. If not provided,\
             write to stdout",
     )
-
     return parser.parse_args()
 
 
@@ -27,6 +39,8 @@ def download_file(url: str, output_path: str) -> None:
     """
     Download `url` to the provided output path.
     """
+    if url == "":
+        return
     with urlopen(url) as response:
         downloaded_contents = response.read().decode()
     if output_path == "":
@@ -37,9 +51,58 @@ def download_file(url: str, output_path: str) -> None:
     print(f"Wrote downloaded content to `{output_path}`")
 
 
+def _get_choice() -> int:
+    """Return user's choice, -1 for quit"""
+    print("Options:\n1. Custom URL\n2. GitHub raw file download\n\n`q` to exit\n")
+    while True:
+        option = input("Choice: ")
+        if option == "q":
+            return -1
+        if not option.isdigit():
+            print("Make sure you enter a number")
+            continue
+        choice = int(option)
+        if choice not in range(1, 2 + 1):
+            print("That's not a valid choice")
+            continue
+        return choice
+
+
+def _raw_github() -> str:
+    """Assist a user in generating a raw github url"""
+    url = "https://raw.githubusercontent.com/"
+    url += input("GitHub account name: ")
+    url += "/"
+    url += input("GitHub repository name: ")
+    url += "/"
+    url += input("Branch name [enter for `main`]: ") or "main"
+    url += "/"
+    url += input("Path to file on GitHub: ")
+    print(f"Download URL is {url}")
+    return url
+
+
+def assistant(path: str) -> tuple[str, str]:
+    """Prompt the user to generate url"""
+    if path == "":
+        path = input("File save path [enter for stdout]: ")
+    choice = _get_choice()
+    if choice == -1:
+        return "", ""
+    if choice == 1:
+        return input("Download URL: "), path
+    if choice == 2:
+        return _raw_github(), path
+    raise ValueError("Invalid choice uncaught in _get_choice")
+
+
 def main(args: Namespace) -> None:
     """Entry point for get.py"""
-    download_file(args.url, args.path)
+    url = args.url
+    path = args.path
+    if args.assist:
+        url, path = assistant(path)
+    download_file(url, path)
 
 
 if __name__ == "__main__":
